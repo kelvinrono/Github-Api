6246a397520d2916ed2c5c19143c8203bbb4829c
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-05-22 14:43:57 +0300

    create github details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GithubDetailsComponent } from './github-details/github-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubDetailsComponent
   ],
   imports: [
     BrowserModule,
