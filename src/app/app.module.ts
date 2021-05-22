a1056d5fb5288d27bc6e3b3659c17449f75ee569
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-05-22 14:56:53 +0300

    set all the necessary files in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import{HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubDetailsComponent } from './github-details/github-details.component';
+import { GithubFormComponent } from './github-form/github-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GithubDetailsComponent
+    GithubDetailsComponent,
+    GithubFormComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
