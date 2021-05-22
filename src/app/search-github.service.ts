9c9232bb308274a9a67e0734a469a9817b80a419
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-05-22 15:15:18 +0300

    add an interface

--- src/app/search-github.service.ts
+++ src/app/search-github.service.ts
@@ -12,5 +12,19 @@ export class SearchGithubService {
   repository!: Repository;
   repoData = [];
   newUserData:any = [];
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.user = new User("", 0, "", "", new Date())
+    this.repository = new Repository("", "", new Date(), "", "")
+  }
+
+    getUserData(username:string){
+      interface ApiResponse {
+        bio: string,
+        public_repos: number,
+        login: string,
+        avatar_url: string,
+        created_at: Date
+  
+      }
+    }
 }
