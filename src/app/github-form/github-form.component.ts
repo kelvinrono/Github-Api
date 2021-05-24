018a50e58eafdf5c86efa0ea53d82e3236deff4d
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-05-24 15:33:09 +0300

    split the section with an horizontal line

--- src/app/github-form/github-form.component.ts
+++ src/app/github-form/github-form.component.ts
@@ -13,7 +13,7 @@ export class GithubFormComponent implements OnInit {
   searchGithubService:SearchGithubService;
   submitUsername(){
     this.searchGithubService.getUserData(this.username)
-    alert('Click on view results to see the fedback')
+    alert('Click on view results to see the fetched data')
   }
   constructor(searchGithubService:SearchGithubService) { 
     this.searchGithubService = searchGithubService;
