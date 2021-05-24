ff1b0379a2fa998f27d2ef60f942fdb951efd514
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-05-24 15:27:09 +0300

    delete daysPast pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,17 @@ import { AppComponent } from './app.component';
 import { GithubDetailsComponent } from './github-details/github-details.component';
 import { GithubFormComponent } from './github-form/github-form.component';
 import { HighlightDirective } from './highlight.directive';
+import { DateCountPipe } from './date-count.pipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     GithubDetailsComponent,
     GithubFormComponent,
-    HighlightDirective
+    HighlightDirective,
+    DateCountPipe,
+ 
   ],
   imports: [
     BrowserModule,
